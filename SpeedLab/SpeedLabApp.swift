4276366b6a8b273008fa957e860544d06fb081d3
Arican <93190017+aarican1@users.noreply.github.com>
2026-01-26 20:23:03 +0300

    Initial Commit

---
+++ SpeedLab/SpeedLabApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeedLabApp.swift
+//  SpeedLab
+//
+//  Created by Abdurrahman Arıcan on 26.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct SpeedLabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
